ddb1c30fa54f45e11faaf57d0671986e31cb491a
piyush.rana <piyush.rana@sakhaglobal.com>
2025-05-10 20:27:22 +0530

    added Header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Explore from "@/components/ExploreCard"
 import StudyAbroadCard from "@/components/StudyAbroadCard/StudyAbroadCard"
 import Supports from "@/components/Supports/Supports"
 import UniversityCarousel from "@/components/UniversityCarousel"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="w-full max-w-md px-4 pt-8 pb-7 bg-[#F0ECFF] rounded-b-[24px]">
+    <main className="w-full overflow-x-hidden flex min-h-screen flex-col items-center justify-center">
+      <Header/>
+      <div className="w-full max-w-md px-4 pt-[90px] pb-7 bg-[#F0ECFF] rounded-b-[24px]">
         <StudyAbroadCard />
       </div>
       <div className="w-full max-w-md px-4 pt-8 pb-7">
@@ -14,6 +18,10 @@ export default function Home() {
       <div className="w-full max-w-md px-4 pt-8 pb-7">
         <UniversityCarousel/>
       </div>
+      <div className="w-full max-w-md px-4 pt-8 pb-7">
+        <Explore/>
+      </div>
+      <Footer/>
     </main>
   )
 }
