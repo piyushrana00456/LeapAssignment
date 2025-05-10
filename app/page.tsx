819b5eb54ad116c7fc68eb60c1e3911ca2b33579
piyush.rana <piyush.rana@sakhaglobal.com>
2025-05-10 16:15:17 +0530

    Imported support and University component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import StudyAbroadCard from "@/components/StudyAbroadCard/StudyAbroadCard"
+import Supports from "@/components/Supports/Supports"
+import UniversityCarousel from "@/components/UniversityCarousel"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="w-full max-w-md px-4 pt-8 pb-7 border-solid bg-[#F0ECFF] rounded-b-[24px]">
+      <div className="w-full max-w-md px-4 pt-8 pb-7 bg-[#F0ECFF] rounded-b-[24px]">
         <StudyAbroadCard />
+      </div>
+      <div className="w-full max-w-md px-4 pt-8 pb-7">
+        <Supports />
+      </div>
+      <div className="w-full max-w-md px-4 pt-8 pb-7">
+        <UniversityCarousel/>
       </div>
     </main>
   )
