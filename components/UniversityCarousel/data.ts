0684c7858f5aebf222ee7049155217fe83e47ef5
piyush.rana <piyush.rana@sakhaglobal.com>
2025-05-10 16:12:38 +0530

    carousel dummy data

---
+++ components/UniversityCarousel/data.ts
@@ -0,0 +1,44 @@
+import type { University } from "./Carousel.types"
+
+export const universities: University[] = [
+  {
+    id: 1,
+    name: "University of Edinburgh",
+    country: "UK",
+    countryCode: "GB",
+    image: "/University.png",
+    admitCount: 150,
+  },
+  {
+    id: 2,
+    name: "University of Toronto",
+    country: "Canada",
+    countryCode: "CA",
+    image: "/University.png",
+    admitCount: 120,
+  },
+  {
+    id: 3,
+    name: "University of Melbourne",
+    country: "Australia",
+    countryCode: "AU",
+    image: "/University.png",
+    admitCount: 85,
+  },
+  {
+    id: 4,
+    name: "Stanford University",
+    country: "USA",
+    countryCode: "US",
+    image: "/University.png",
+    admitCount: 200,
+  },
+  {
+    id: 5,
+    name: "University of Oxford",
+    country: "UK",
+    countryCode: "GB",
+    image: "/University.png",
+    admitCount: 175,
+  },
+]
